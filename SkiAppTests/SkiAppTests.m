5b1b674170bd2159d288e83896f909bf09058f14
Kovacs Kristof <kovacskristof200@gmail.com>
2016-04-05 16:02:12 +0300

    Update

---
+++ SkiAppTests/SkiAppTests.m
@@ -0,0 +1,39 @@
+//
+//  SkiAppTests.m
+//  SkiAppTests
+//
+//  Created by Kovács Kristóf on 19/02/16.
+//  Copyright © 2016 Kovács Kristóf. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SkiAppTests : XCTestCase
+
+@end
+
+@implementation SkiAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
